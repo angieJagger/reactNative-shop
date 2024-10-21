33ce24a511d1e1a6536758fe72ed2abf0f893052
angieJagger <cojeszczenowego@gmail.com>
2024-10-21 21:07:42 +0200

    creating React Native Section with routing, layouts, cart state, place on orders or order details page

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  FlatList,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import { ProductListItem } from '../../components/product-list-item';
+import { ListHeader } from '../../components/list-header';
+import { PRODUCTS } from '../../../assets/products';
 
 
-const index = () => {
+const Home = () => {
   return (
     <View>
-      <Text>Hello</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: 'space-between',
+  },
+});
